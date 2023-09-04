a5115f9f4f20d69997fa288eb622a2256543f382
Rex Cheng <hkchengrex@gmail.com>
2023-09-04 12:47:16 -0500

    update examples

--- scripts/download_models.sh
+++ scripts/download_models.sh
@@ -2,3 +2,4 @@ wget -P ./saves/ https://github.com/hkchengrex/Tracking-Anything-with-DEVA/relea
 wget -P ./saves/ https://github.com/IDEA-Research/GroundingDINO/releases/download/v0.1.0-alpha/groundingdino_swint_ogc.pth
 wget -P ./saves/ https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth
 wget -P ./saves/ https://github.com/hkchengrex/Tracking-Anything-with-DEVA/releases/download/v1.0/mobile_sam.pt
+wget -P ./saves/ https://github.com/hkchengrex/Tracking-Anything-with-DEVA/releases/download/v1.0/GroundingDINO_SwinT_OGC.py
