5a966bf62efbf980f44e93324f157ffd94dd9d3a
Rex Cheng <hkchengrex@gmail.com>
2023-09-04 12:30:58 -0500

    update download links

--- scripts/download_models.sh
+++ scripts/download_models.sh
@@ -1,4 +1,4 @@
 wget -P ./saves/ https://github.com/hkchengrex/Tracking-Anything-with-DEVA/releases/download/v1.0/DEVA-propagation.pth
 wget -P ./saves/ https://github.com/IDEA-Research/GroundingDINO/releases/download/v0.1.0-alpha/groundingdino_swint_ogc.pth
 wget -P ./saves/ https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth
-wget -P ./saves/ https://raw.githubusercontent.com/ChaoningZhang/MobileSAM/master/weights/mobile_sam.pt
+wget -P ./saves/ https://github.com/hkchengrex/Tracking-Anything-with-DEVA/releases/download/v1.0/mobile_sam.pt
